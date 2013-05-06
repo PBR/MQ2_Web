9d19100e33c8a990bb0d6329e9f0d4cf53ffcb29
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-05-06 12:49:12 +0200

    Make the bar a little larger which helps getting the tooltip showing correctly

--- mq2_web.py
+++ mq2_web.py
@@ -679,7 +679,7 @@ def results(session_id, exp_id):
          "data": data_qtls,
          "bars": {
                 "show": 1,
-                "barWidth": 0.5,
+                "barWidth": 0.6,
                 "order": 1,
             },
          },
@@ -688,7 +688,7 @@ def results(session_id, exp_id):
          "data": data_lg,
          "bars": {
                 "show": 1,
-                "barWidth": 0.1,
+                "barWidth": 0.4,
                 "order": 2,
             },
          },
